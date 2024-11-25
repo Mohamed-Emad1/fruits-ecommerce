8835f0cdac476c421e4eb8826d700c31d40d3617
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-11-25 23:02:23 +0200

    splash view done

--- lib/core/helper_functions/on_generate_rout.dart
+++ lib/core/helper_functions/on_generate_rout.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fruitshup/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:fruitshup/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (_) => const SplashView());
+
+    case OnboardingView.routeName:
+      return MaterialPageRoute(builder: (_) => const OnboardingView());
     default:
       return MaterialPageRoute(builder: (_) => const Scaffold());
   }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruitshup/core/utils/app_images.dart';
+import 'package:fruitshup/features/onboarding/presentation/views/onboarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    executeNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        SvgPicture.asset(Assets.imagesPlant),
-        SvgPicture.asset(Assets.imagesAppIcon),
-        // SvgPicture.asset(Assets.imagesSplashCircles),
-
+        // Text("SplashViewBody"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        Center(child: SvgPicture.asset(Assets.imagesLogo)),
+        SvgPicture.asset(
+          Assets.imagesSplashCircles,
+          fit: BoxFit.fill,
+        ),
       ],
     );
+  }
+  
+  void executeNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnboardingView.routeName);
+    });
   }
 }
